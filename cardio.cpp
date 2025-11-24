e4efa4555e89bbc5b621e74fecdce0b26a103cc7
Arthur Rosado <aarthurrosado@gmail.com>
2025-11-24 09:58:01 -0300

    Update cardio.cpp

--- cardio.cpp
+++ cardio.cpp
@@ -1,44 +1,53 @@
 #include "Cardio.h"
 #include <iostream>
+#include <iostream>
+using namespace std;
 
 // Construtor para novos cadastros
 Cardio::Cardio(std::string nome, int duracao, double caloriasPorMinuto) 
     : Exercicio(nome) {
-    // Implementar
+    this->duracao = duracao;
+    this->caloriasPorMinuto = caloriasPorMinuto;
+    // check
 }
 
 // Construtor para leitura de arquivo
 Cardio::Cardio(int id, std::string nome, bool ativo, int duracao, double caloriasPorMinuto)
     : Exercicio(id, nome, ativo) {
-    // Implementar
+    // check
 }
 
 // Exibir detalhes do exercício
 void Cardio::exibirDetalhes() const {
-    // Implementar
-}
+    cout << "Tipo: Cardio" << endl;
+    cout << "ID: " << id << endl;
+    cout << "Cardio: " << nome << endl;
+    cout << "Status: " << (ativo ? "Ativo" : "Inativo ")<< endl;
+    cout << "Duracao " << duracao << endl;
+    cout << "CaloriasPorMinuto: " << caloriasPorMinuto << endl;
+} // check
 
 // Calcular tempo estimado (duração em minutos)
 double Cardio::calcularTempoEstimado() const {
-    return 0.0; // Implementar
+    return duracao; // check
 }
 
 // Calcular calorias gastas
 double Cardio::calcularCaloriasGastas() const {
-    return 0.0; // Implementar
+    return duracao * caloriasPorMinuto; // check
 }
 
 // Retornar tipo do exercício (1 = Cardio)
 int Cardio::getTipo() const {
-    return 0; // Implementar
+    return 1; 
 }
 
 // Getter de duração
 int Cardio::getDuracao() const {
-    return 0; // Implementar
+    return duracao; // check
 }
 
 // Getter de calorias por minuto
 double Cardio::getCaloriasPorMinuto() const {
-    return 0.0; // Implementar
+    return caloriasPorMinuto; 
 }
